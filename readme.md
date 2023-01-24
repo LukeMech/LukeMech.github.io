3ce41a303af4e9d9f5d4e48356862955440ba768
Bajojajo-xD <81306360+LukeMech@users.noreply.github.com>
2023-01-24 12:42:58 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# 🪄 My main site
+
+## 🚀 Hosted [here](https://lukemech.github.io/)
+
+## 🔥 This repository contains:
+- 🪢 Source code for website
---
+++ scripts/announcment.js
@@ -0,0 +1,22 @@
+var globalAnnouncment = 
+
+//HERE WRITE ANNOUNCMENT
+'<i class="fa-solid fa-bell"></i> Site under reconstruction'
+
+
+
+//HERE WRITE ADDITIONAL ANNOUNCMENTS FOR SPECIFIED FILES
+var mainAnnouncment = '',
+    aboutAnnouncment = '',
+    docsAnnouncment = '';
+
+
+var fileName = location.href.split("/").slice(-1), text;
+var announcment = document.getElementById("announcment")
+console.log(fileName[0])
+if(fileName[0] == 'docs.html') text = docsAnnouncment
+else if (fileName[0] == 'about.html') text = aboutAnnouncment
+else if (fileName[0] == 'index.html') text = mainAnnouncment
+text ? (globalAnnouncment ? text = text + '<br />' + globalAnnouncment : text = text) : (globalAnnouncment ? text = globalAnnouncment : text = '')
+
+announcment.innerHTML = text
